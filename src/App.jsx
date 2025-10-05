5cfa4de2e5a8bc9266f9e5d86af399ba483acf36
CeToribio <anctoribio@gmail.com>
2025-10-05 15:51:49 -0500

    fix: error building no exist import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import FloatingSun from "./components/FloatingSun";
 import Starfield from "./components/Starfield";
 
 export default function App() {
